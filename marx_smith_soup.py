43182046619b432d59caa4ab49fbb507b068620a
jburnford <cljim22@gmail.com>
2015-04-27 10:51:23 +0200

    added Hit Enter to Continue throughout.

--- marx_smith_soup.py
+++ marx_smith_soup.py
@@ -58,42 +58,50 @@ print "WORDS:"
 text = nltk.Text(tokens)
 words = [w.lower() for w in text]
 print words[100:200]
-
+raw_input("Press Enter to continue...")
 #Our word list, however, still contains common English worlds "the", "of", "a", "to" and punctuation. This next step
 #uses our stopword list created above to strip the list of words. I've also only returned words larger than 1 characters
 #as I noticed some OCR noise showing up in the word frequencies. The final step prints a random selection of filtered
 #words from the 500th to the 600th in the list.
+
 print "FILTERED WORDS"
 filtered_words = [w for w in words if not w in stopword]
 #filtered_words = [w for w in filtered_words if not w in punk]
 filtered_words = [w for w in filtered_words if len(w) > 1]
 print filtered_words[500:600]
+raw_input("Press Enter to continue...")
 
 #This step uses an NLTK function to determine the most frequent words in the filtered list:
 print "WORD FREQUENCY:"
 print nltk.FreqDist(filtered_words)
+raw_input("Press Enter to continue...")
 
 #It is also possible to tokenize the sentences and this step is useful for further natural language analysis. The third
 #step prints ten sentences from the middle of the text.
 print "SENTENCES:"
 sents = sent_tokenizer.tokenize(raw)
 print sents[171:181]
+raw_input("Press Enter to continue...")
 
 #This step produces a vocabulary of words used in the text by discarding all duplicates and sorting the results.
 print "VOCAB:"
 vocab = sorted(set(filtered_words))
 print vocab[500:600]
+raw_input("Press Enter to continue...")
 
 #The Collocations command finds the most common word pairs in the text. You'll notice we still have some OCR issues
 #as a few split words show up in the collocations results "commod ities":
 print "COLLOCATIONS:"
 print text.collocations()
+raw_input("Press Enter to continue...")
 
 #Concordance is a really powerful function that displays keywords in context. It give us a quick way to check how the
 #text uses words like freedom or nature. Feel free to add a third concordance:
 print "CONCORDANCE:"
 print text.concordance('freedom')
+raw_input("Press Enter to continue...")
 print text.concordance('nature')
+raw_input("Press Enter to continue...")
 
 #This function adds part of speech tags to 801 word tokens. This is a slow, computationally intensive, task, so
 #I limited to a small sample for this workshop. This is an important step in an NLP pipeline, as more complicated
@@ -102,10 +110,12 @@ print text.concordance('nature')
 print "PART OF SPEECH TAGGED:"
 tagged = nltk.pos_tag(tokens[200:1000])
 print tagged
+raw_input("Press Enter to continue...")
 
 #This searches through the text and provides us with the words found before the word man in the textL
 print "WORDS FOUND BEFORE _MAN_"
 print text.findall(r" (<.*>) <man>")
+raw_input("Press Enter to continue...")
 
 #This final function creates a text dispersion graph that shows us where different words appear in the text:
 text.dispersion_plot(["labour", "democracy", "freedom", "capital", "nature"])
@@ -113,7 +123,7 @@ text.dispersion_plot(["labour", "democracy", "freedom", "capital", "nature"])
 #Same again for Adam Smith
 print "Adam Smith Wealth of Nations"
 soup = BeautifulSoup(urllib2.urlopen("https://archive.org/stream/WealthOfNationsAdamSmith/Wealth%20of%20Nations_Adam%20Smith_djvu.txt").read())
-
+raw_input("Press Enter to continue...")
 
 raw = soup.pre.string
 #I checked the source HTML code and found that the <pre> tag comes after all of the website preamble that we don't
@@ -135,6 +145,7 @@ print "WORDS:"
 text = nltk.Text(tokens)
 words = [w.lower() for w in text]
 print words[100:200]
+raw_input("Press Enter to continue...")
 
 #Our word list, however, still contains common English worlds "the", "of", "a", "to" and punctuation. This next step
 #uses our stopword list created above to strip the list of words. I've also only returned words larger than 1 characters
@@ -145,32 +156,39 @@ filtered_words = [w for w in words if not w in stopword]
 #filtered_words = [w for w in filtered_words if not w in punk]
 filtered_words = [w for w in filtered_words if len(w) > 1]
 print filtered_words[500:600]
+raw_input("Press Enter to continue...")
 
 #This step uses an NLTK function to determine the most frequent words in the filtered list:
 print "WORD FREQUENCY:"
 print nltk.FreqDist(filtered_words)
+raw_input("Press Enter to continue...")
 
 #It is also possible to tokenize the sentences and this step is useful for further natural language analysis. The third
 #step prints ten sentences from the middle of the text.
 print "SENTENCES:"
 sents = sent_tokenizer.tokenize(raw)
 print sents[171:181]
+raw_input("Press Enter to continue...")
 
 #This step produces a vocabulary of words used in the text by discarding all duplicates and sorting the results.
 print "VOCAB:"
 vocab = sorted(set(filtered_words))
 print vocab[500:600]
+raw_input("Press Enter to continue...")
 
 #The Collocations command finds the most common word pairs in the text. You'll notice we still have some OCR issues
 #as a few split words show up in the collocations results "commod ities":
 print "COLLOCATIONS:"
 print text.collocations()
+raw_input("Press Enter to continue...")
 
 #Concordance is a really powerful function that displays keywords in context. It give us a quick way to check how the
 #text uses words like freedom or nature. Feel free to add a third concordance:
 print "CONCORDANCE:"
 print text.concordance('freedom')
+raw_input("Press Enter to continue...")
 print text.concordance('nature')
+raw_input("Press Enter to continue...")
 
 #This function adds part of speech tags to 801 word tokens. This is a slow, computationally intensive, task, so
 #I limited to a small sample for this workshop. This is an important step in an NLP pipeline, as more complicated
@@ -179,10 +197,12 @@ print text.concordance('nature')
 print "PART OF SPEECH TAGGED:"
 tagged = nltk.pos_tag(tokens[200:1000])
 print tagged
+raw_input("Press Enter to continue...")
 
 #This searches through the text and provides us with the words found before the word man in the textL
 print "WORDS FOUND BEFORE _MAN_"
 print text.findall(r" (<.*>) <man>")
+raw_input("Press Enter to continue...")
 
 #This final function creates a text dispersion graph that shows us where different words appear in the text:
 text.dispersion_plot(["labour", "democracy", "freedom", "capital", "nature"])
